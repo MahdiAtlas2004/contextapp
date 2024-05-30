12d14d5d12c1fcdefcfbc18a922db119c341cc85
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-05-30 09:45:03 +0430

    BookContext added.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
 import AuthContextProvider from "./contexts/AuthContext";
+import BookContextProvider from "./contexts/BookContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-      <AuthContextProvider>
+        <AuthContextProvider>
           <Navbar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggle />
-      </AuthContextProvider>    
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,18 +1,22 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { BookContext } from "../contexts/BookContext";
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
+  const { books } = useContext(BookContext);
   return (
     <div
       className="book-list"
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>The way of kings</li>
-        <li style={{ background: theme.ui }}>The name of wind</li>
-        <li style={{ background: theme.ui }}>The final empire</li>
+        {books.map((book) => (
+          <li style={{ background: theme.ui }} key={book.id}>
+            {book.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
